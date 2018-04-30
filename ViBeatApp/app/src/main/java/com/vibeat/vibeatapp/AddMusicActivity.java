4a3b8686c256ff25c90f3d0f878c68ac3e158abd
idanccohen <38830897+idanccohen@users.noreply.github.com>
2018-04-30 21:55:41 +0300

    added addMusicActivity

---
+++ ViBeatApp/app/src/main/java/com/vibeat/vibeatapp/AddMusicActivity.java
@@ -0,0 +1,13 @@
+package com.vibeat.vibeatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddMusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_music);
+    }
+}
