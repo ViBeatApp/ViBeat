c5b94ed80f1412b6f491f7967eafac0326a9a97b
Tomer Solomon <tomer.solomon1@gmail.com>
2018-05-20 18:35:09 +0300

    test

--- serverModule/test.java
+++ serverModule/test.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.json.JSONException;
 
 public class test {
-	public static void main(String[] args) throws JSONException, IOException {
+	public static void main(String[] args) throws Exception {
 		//check_enum();
 		SocketChannel socket = SocketChannel.open(new InetSocketAddress("localhost", 9999));
 		System.out.println("create new party");
@@ -19,34 +19,34 @@ public class test {
 		
 		Command create = new Command(CommandType.CREATE);
 		create.cmd_info.put("NAME", "Ido's party");
-		create.cmd_info.put("private", false);
+		create.cmd_info.put("IS_PRIVATE", false);
 		readWriteAux.writeSocket(socket, create);
-		System.out.println(readWriteAux.readSocket(socket));
+		//System.out.println(readWriteAux.readSocket(socket));
+		check_manage_songs(socket);
 	}
 	
 	public static void check_manage_songs(SocketChannel socket) throws Exception {
 		Command add_song = new Command(CommandType.ADD_SONG);
 		Command reply;
+		System.out.println("sending new_command");
 		add_song.cmd_info.put("URL", "www.youtube1");
 		readWriteAux.writeSocket(socket, add_song);
+		System.out.println("send URL1");
 		reply = readWriteAux.readSocket(socket);
+		System.out.println("got reply");
 		System.out.println(reply);
 		
 		add_song.cmd_info.put("URL", "www.youtube2");
 		readWriteAux.writeSocket(socket, add_song);
 		reply = readWriteAux.readSocket(socket);
+		System.out.println("got reply");
 		System.out.println(reply);
 		
 		add_song.cmd_info.put("URL", "www.youtube3");
 		readWriteAux.writeSocket(socket, add_song);
 		reply = readWriteAux.readSocket(socket);
+		System.out.println("got reply");
 		System.out.println(reply);
-	}
-	
-	private static void check_enum() {
-		CommandType cmd = CommandType.valueOf("PLAY_SONG");
-		System.out.println("cmd = " + cmd.name()); 
-		
 	}
 
 	public static void printInfo(Party party){
