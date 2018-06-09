0c8541d8cc9c4690f6f3473047a42c91513a6251
DANA OSHRI <dana.oshri@gmail.com>
2018-06-09 19:54:43 +0200

    same

--- ViBeatApp/app/src/main/java/com/vibeat/vibeatapp/Managers/MediaPlayerManager.java
+++ ViBeatApp/app/src/main/java/com/vibeat/vibeatapp/Managers/MediaPlayerManager.java
@@ -33,14 +33,17 @@ public class MediaPlayerManager {
                 return; //ERROR
             } else if (num_tracks == 1) {
                 if (track_id == m1.track_id) {
+                    m2.reset();
                     m1.getReady(track_id, offset);
                     active_mp = 1;
                 }
                 else if (track_id == m2.track_id) {
+                    m1.reset();
                     m2.getReady(track_id, offset);
                     active_mp = 2;
                 }
                 else{
+                    m2.reset();
                     m1.getReady(track_id, offset);
                     active_mp = 1;
                 }
