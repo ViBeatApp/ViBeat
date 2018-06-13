6aeea27c29fce2387e0c3751c9017e57240f6ac2
Tomer Solomon <tomer.solomon1@gmail.com>
2018-06-13 11:26:49 +0300

    fix Images

--- serverModule/serverModule/Party_thread.java
+++ serverModule/serverModule/Party_thread.java
@@ -482,16 +482,16 @@ public class Party_thread implements Runnable {
 			return;
 		}
 		System.out.println("party thread - updatePartyToAll were updates");
-		//SendCommandToAll(party.update_party);
+		SendCommandToAll(party.update_party);
 		party.update_party = new Command(CommandType.SYNC_PARTY);
-		syncPartyToAll();
+		//syncPartyToAll();
 	}
 	
-	private void syncPartyToAll() throws JSONException, IOException {
+	/*private void syncPartyToAll() throws JSONException, IOException {
 		JSONObject party_info = party.getFullJson();
 		Command sync_command = Command.create_syncParty_Command(party_info);
 		SendCommandToAll(sync_command);
-	}
+	}*/
 
 	/* updates the GetReady command */
 	public void update_get_ready_command() throws JSONException {	
