41ffbdb7b2f3a1a13523f5a750c04781f7d927cc
DANA OSHRI <dana.oshri@gmail.com>
2018-06-13 13:27:29 +0200

    few changes extra

--- ViBeatApp/app/src/main/java/com/vibeat/vibeatapp/imageLoader.java
+++ ViBeatApp/app/src/main/java/com/vibeat/vibeatapp/imageLoader.java
@@ -1,19 +1,16 @@
 package com.vibeat.vibeatapp;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.vibeat.vibeatapp.HelperClasses.MyTransformation;
 
 import java.util.List;
 
 public class imageLoader {
-    public static void loadImage(final Activity act, final List<String> img_paths,final List<ImageView> views, final int color){
+    public static void loadImage(final Activity act, final List<String> img_paths,final List<ImageView> views){
         act.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -25,40 +22,27 @@ public class imageLoader {
 
         new Thread() {
             public void run() {
-                for (int i = 0; i < img_paths.size(); ++i){
+                for (int i = 0; i < img_paths.size(); ++i) {
                     try {
                         final int index = i;
                         act.runOnUiThread(new Runnable() {
                             public void run() {
-                                /*Glide.with(act)
+                                Glide.with(act)
                                     .load(img_paths.get(index))
-                                    .transform(new MyTransformation(act, color))
+                                    .transform(new MyTransformation(act))
                                     .error(R.drawable.cardib)
-                                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                    //.diskCacheStrategy(DiskCacheStrategy.ALL)
                                     .placeholder(R.drawable.blank)
                                         .dontAnimate()
-                                    .into(views.get(index));*/
-                                Glide.with(act)
-                                        .load(img_paths.get(index))
-                                        .asBitmap()
-                                        .centerCrop()
-                                        .into(new BitmapImageViewTarget(views.get(index)) {
-                                    @Override
-                                    protected void setResource(Bitmap resource) {
-                                        RoundedBitmapDrawable circularBitmapDrawable =
-                                                RoundedBitmapDrawableFactory.create(act.getResources(), resource);
-                                        circularBitmapDrawable.setCircular(true);
-                                        views.get(index).setImageDrawable(circularBitmapDrawable);
-                                    }
-                                });
+                                    .into(views.get(index));
                             }
                         });
-                    }
-                    catch (Exception e) {
+                    } catch (Exception e) {
                         Log.d("image", "failed: " + i);
-                        e.printStackTrace(); }
+                        e.printStackTrace();
+                    }
                 }
-                }
+            }
         }.start();
 
     }
